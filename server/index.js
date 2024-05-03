6f3ff0f08f862ed30df12f1f49e6859808019709
philippark <philippark271@gmail.com>
2024-05-03 13:30:59 -0400

    update todo route

--- server/index.js
+++ server/index.js
@@ -46,7 +46,19 @@ app.get("/todos/:id", async(req, res)=>{
 })
 
 //update todo
+app.put("/todos/:id", async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
 
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+
+        res.json("Todo was updated");
+
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 //delete a todo
 
