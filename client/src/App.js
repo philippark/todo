a618769f1e313815fbf7686a4ae234f479f15b55
philippark <philippark271@gmail.com>
2024-05-03 16:31:37 -0400

    init input form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
+import InputTodo from './components/InputTodo';
 
 import './App.css';
 
 function App() {
   return (
     <Fragment>
+      <div className='container'>
+        <InputTodo/>
+      </div>
       
     </Fragment>
   );
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,19 @@
+import react, {Fragment, useState} from "react";
+
+const InputTodo = () => {
+    const [description, setDescription] = useState("");
+
+    return ( 
+        <Fragment>
+            <h1 className = "text-center mt-5">Pern Todo List </h1>    
+            <form className="d-flex mt-5">
+                <input type="text" className="form-control" value={description} onChange={e=>{
+                    setDescription(e.target.value)
+                }}/>
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default InputTodo;
