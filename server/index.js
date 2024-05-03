54405fb3b7c6ea148f428e705c788c8fd12ef3f6
philippark <philippark271@gmail.com>
2024-05-02 21:56:11 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+//create a todo
+app.post("/todos", async(req, res)=>{
+    try {
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO (description) VALUES($1)", [description]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+
+app.listen(5000, ()=>{
+    console.log("Server has started on port 5000");
+});
