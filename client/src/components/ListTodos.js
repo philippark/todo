33235191f9926010b0b214fc74d40146b10f3426
philippark <philippark271@gmail.com>
2024-05-04 19:38:57 -0400

    delete button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -16,6 +16,19 @@ const ListTodos = () => {
         }
     };
 
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE",
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     useEffect(()=>{
         getTodos();
     }, []);
@@ -30,25 +43,16 @@ const ListTodos = () => {
                     <th>Delete</th>
                 </tr>
                 </thead>
-
-                {todos.map(todo=>(
-                    <tr>
-                        <th>{todo.description}</th>
-                        <th><button>Edit</button></th>
-                        <th><button>Delete</button></th>
-                    </tr>
-                ))}
-                {/* 
-
-                <tbody>
-                <tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>john@example.com</td>
-                </tr>
-                </tbody>
                 
-                */}
+                <tbody>
+                    {todos.map(todo=>(
+                        <tr key = {todo.todo_id}>
+                            <th>{todo.description}</th>
+                            <th><button>Edit</button></th>
+                            <th><button className="btn btn-danger" onClick={()=>deleteTodo(todo.todo_id)}>Delete</button></th>
+                        </tr>
+                    ))}
+                </tbody>
                 
             </table>
         </Fragment>
